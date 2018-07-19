c0ec01e94ff77e25fecc371080576dbf8a5a0302
Michael <vmichaelkirkaldy@live.com>
2018-07-19 09:13:00 -0500

    Updates to all files

---
+++ apps/reviewer/models.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import messages
+from django.db import models
+
+class UserManager(models.Manager):
+	def validate_user(request, postData):
+		errors = {}
+
+		#Name validation
+		if len(postData['name']) < 2:
+			errors['name'] = "Name must be 2 characters or more in length"
+
+		#Email validation
+
+
+		#Password Validation
+		if len(postData['password']) < 8:
+			errors['password'] = "Password must be at least 8 characters"
+
+		#Password match validation
+		if postData['password'] != postData['cnfrm_password']:
+			errors['password'] = "Passwords do not match"
+
+		return errors
+
+class BooksManager(models.Manager):
+	def validate_book(request, postData):
+		errors = {}
+
+		#Title validation
+		if len(postData['title']) < 2:
+			error['title'] = "Title name is too short"
+
+		if len(postData['desc']) < 10:
+			error['desc'] = "The summary must be more than 10 characters long"
+
+		return errors
+
+class ReviewManager(models.Manager):
+	def validate_review(request, postData):
+		errors = {}
+
+		if len(postData['title']) < 2:
+			error['title'] = "The title is too short"
+
+		if len(postData['content']) < 10:
+			error['content'] = "Content must be more than 10 characters long"
+
+		return errors
+
+
+
+
+class Users(models.Model):
+	#Django has the id field set to Autofield. No need to declare one in the model.
+	name = models.CharField(max_length=255)
+	alias = models.CharField(max_length=255)
+	email = models.CharField(max_length=255)
+	password = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	objects = UserManager()
+
+class Books(models.Model):
+	#Django has the id field set to Autofield. No need to declare one in the model.
+	title = models.CharField(max_length=255)
+	desc = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	objects = BooksManager()
+
+class Review(models.Model):
+	#Django has the id field set to Autofield. No need to declare one in the model.
+	title = models.CharField(max_length=255)
+	content = models.CharField(max_length=255)
+	rating = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	book = models.ForeignKey(Books, related_name="reviews")
+	user = models.ForeignKey(Users, related_name="reviews")
+	objects = ReviewManager()
+
---
+++ apps/reviewer/views.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect, HttpResponse
+import bcrypt
+from .models import *
+
+
+def index(request):
+	return render(request, "reviewer/index.html.")
+
+def register(request):
+	errors = Users.objects.validate_user(request.POST)
+
+	if len(errors):
+		for tag, error in errors.iteritems():
+			messages.error(request, error)
+			return redirect('/')
+
+
+	#assigns post data to a variable. 
+	name = request.POST['name']
+	alias = request.POST['alias']
+	email = request.POST['email']
+	password = request.POST['password']
+
+	#generates a hashed password and generates a salt before being saved into the database. 
+	#.encode turns it into a usable string
+	hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+	#Saves password as the hashed password! not the password itself. 
+	Users.objects.create(name=name, alias=alias, email=email, password=hashed_pw)
+
+	#gets email, where email is request.POST['email'], stores new entry to the variable.
+	user = Users.objects.get(email=email)
+	#sets said entries id to a session id.
+	#Django has the id field set to Autofield. No need to declare one in the model.
+	request.session['id'] = user.id
+
+	return redirect('/books')
+
+def login(request):
+
+	email = request.POST['email']
+	password = request.POST['password']
+	
+	#gets email where email 
+	user = Users.objects.filter(email=email)
+
+	
+	
+	if len(user) > 0:
+		this_password = bcrypt.checkpw(password.encode(), user[0].password.encode())
+		if this_password:
+			request.session['id'] = user[0].id
+			return redirect('/books')
+		else:
+			messages.error(request, "Incorrect username/password combination.")
+			return redirect('/')
+
+	else:
+		messages.error(request, "User does not exist.")
+		return redirect('/')		
+
+def books(request):
+	return render(request, 'reviewer/books.html')
+
